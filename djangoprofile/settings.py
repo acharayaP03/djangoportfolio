5473f64e22c3a444575fc4779223192c0f2c0b21
acharyaP03 <acharyap03@gmail.com>
2020-12-22 16:53:43 +1100

    about model and migration

--- djangoprofile/settings.py
+++ djangoprofile/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'about.apps.AboutConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# tell django to save all images files 
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
